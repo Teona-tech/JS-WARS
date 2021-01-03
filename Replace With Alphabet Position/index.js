4cf70eb41a15416e9d3e0352b72519fd978993a6
Teona-tech <teoforchxidze30@gmail.com>
2021-01-03 18:54:37 +0400

    სატესტო სჭირდება გაუმჯობესება

---
+++ Replace With Alphabet Position/index.js
@@ -0,0 +1,27 @@
+//Replace With Alphabet Position
+function alphabetPosition(text) {
+    var letters = 'abcdefghijklmnopqrstuvwxyz'.split("");
+    var data = {};
+    var index = 0;
+    for (i of letters){
+        index++;
+        data[i]=index;
+    }
+
+    var asoebi = Object.keys(data);
+    var dashlili =(text.split(""));
+    var dasabrunebeli = "";
+
+    for (each of dashlili){
+        each = each.toLowerCase()
+
+        if (asoebi.includes(each)){
+            dasabrunebeli+=" "+data[each];
+        }
+    }
+    return dasabrunebeli;
+
+    
+}
+var texta = "The sunset sets at twelve o' clock.";
+alphabetPosition(texta);
