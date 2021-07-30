4c23cace597b2c609b4f8dbdc608893190c0da04
Teona-tech <teoforchxidze30@gmail.com>
2021-07-30 13:29:24 +0400

    add phoneNumber task

---
+++ Create Phone Number/phoneNumber.js
@@ -0,0 +1,23 @@
+var numbers = [1,2,3,4,5,6,7,8,9,0];
+
+function createPhoneNumber(numbers){
+    if (Array.isArray(numbers) === false){
+        throw Error("must be an array");
+    }
+
+    if (!numbers.every(function(e){return Number.isInteger(e)})){
+        throw Error ("array must contain integers");
+    }
+   
+    numbers.unshift("(");
+    numbers.splice(4,0,") ");
+    numbers.splice(8,0,"-");
+
+    return numbers.join("");
+    
+}
+
+
+
+createPhoneNumber(numbers);
+
