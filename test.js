2acafc15406ac4661d8eac2da7c8cbe13784b1e5
Teona-tech <teoforchxidze30@gmail.com>
2021-08-01 13:59:54 +0400

    that's how the ternery works

---
+++ test.js
@@ -0,0 +1,9 @@
+let answer = function(num){
+    var num = (num > 0)?  "num is more than 0":
+    (num < 0)? "num is less than 0":
+    (num === 0)? "num eqauls 0":
+    "what the fuck";
+    return num
+}
+
+console.log(answer("fck u"));
