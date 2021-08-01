c31634e8bf72adf87250b972b6cd96e8a659f999
Teona-tech <teoforchxidze30@gmail.com>
2021-08-01 20:41:16 +0400

    rgb to hex converter

---
+++ 6 kyu/RGB To Hex Conversion/task.js
@@ -0,0 +1,14 @@
+function rgb(r, g, b) {
+  const answer = function(val) {
+    if(val < 0) {
+      return '00';
+    }
+    if(val > 255) {
+      return 'FF';
+    }
+    return (val > 15 ? val.toString(16) : '0' + val.toString(16)).toUpperCase();
+  }
+  return answer(r) + answer(g) + answer(b); 
+}
+
+console.log(rgb(0, 0, 0));
